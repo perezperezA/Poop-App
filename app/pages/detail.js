b18f2a2a9fe3bddfe6b69836396c265cd1db85e9
perezperezA <alejandro.perezvandenbosch@gmail.com>
2022-08-23 00:42:02 +0200

    Revert "Heart Rate fail"
    This reverts commit cd35c0e2d4bccd10ebc42ba625912dcc22d0872a.

--- app/pages/detail.js
+++ app/pages/detail.js
@@ -1,8 +1,6 @@
 import document from 'document';
 import { getLocationName } from '../commands';
 import { getStateItem, setStateCallback, removeStateCallback, setStateItem, } from '../state';
-import { HeartRateSensor } from "heart-rate";
-import { me as appbit } from "appbit";
 
 let $button = null;
 let $locationName = null;
@@ -25,18 +23,10 @@ export function destroy() {
 export function init() {
   console.log('init detail page');
   $locationName = document.getElementById('location');
-  if (HeartRateSensor && appbit.permissions.granted("access_heart_rate")) {
-    const hrm = new HeartRateSensor({ frequency: 1 });
-    hrm.addEventListener("reading", () => {
-      console.log('Current heart rate: ${hrm.heartRate}');
-    });
-    hrm.start();
-  }
 
 
-var heartRate = document.getElementById('heartRate');
-  heartRate.text = ('hello world');
-var poopbutton = document.getElementById('poopButton');
+
+var poopbutton = document.getElementById('clicker');
   const amountOfPoops = getStateItem('amountOfPoops');
 document.getElementById('poopstaken').text = getStateItem('amountOfPoops');
   poopbutton.onclick = () => {
